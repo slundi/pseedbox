a923d5db731005d1d75010b643b978e629231876
slundi <informatique@edalis.net>
2018-07-13 23:39:51 +0200

    obfuscate/deobfuscate string (for tracker account password)

---
+++ modules/strings.py
@@ -0,0 +1,19 @@
+#Vigenere cipher
+import base64, binascii
+
+def encode(key, clear):
+    enc = []
+    for i in range(len(clear)):
+        key_c = key[i % len(key)]
+        enc_c = chr((ord(clear[i]) + ord(key_c)) % 256)
+        enc.append(enc_c)
+    return base64.urlsafe_b64encode(bytearray("".join(enc), 'utf-8'))
+
+def decode(key, enc):
+    dec = []
+    enc = (base64.urlsafe_b64decode(enc)).decode("utf-8") 
+    for i in range(len(enc)):
+        key_c = key[i % len(key)]
+        dec_c = chr((256 + ord(enc[i]) - ord(key_c)) % 256)
+        dec.append(dec_c)
+    return "".join(dec)
