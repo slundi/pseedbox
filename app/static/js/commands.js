848e5b33a63f4138deee9274f1b6bf1a5bfa7864
slundi <slundi@gmail.com>
2019-03-10 19:58:22 +0100

    Add empty JS events + minor changes/fixes

--- app/static/js/commands.js
+++ app/static/js/commands.js
@@ -17,11 +17,30 @@ function download_torrent(t) { socket.emit('download_torrent', {torrent: t}); }
 function edit_torrent(t, url, i, p) { socket.emit('download_torrent', {torrent: t, tracker: url, info: i, priv: p}); }
 
 function start_torrent(t) { socket.emit('start_torrent', {torrent: t}); }
+function pause_torrent(t) { socket.emit('pause_torrent', {torrent: t}); }
 function stop_torrent(t) { socket.emit('stop_torrent', {torrent: t}); }
 
+function set_priority(t, priority=2) {/* Priority from 0 to 3 */}
 function set_ratio(t, ratio = 1) { socket.emit('set_ratio', {torrent: t, ratio: ratio}); }
 function set_speed(t, download = undefined, upload = undefined) { socket.emit('set_speed', {torrent: t, download: download, upload: upload}); }
 
 function sort_torrent(t, category) { socket.emit('sort_torrent', {torrent: t, category: category}); }
 
 function set_file_priority(t, file, priority) { socket.emit('set_file_priority', {torrent: t, file: f, priority: priority}); }
+
+function label_torrent(t) {}
+function create_torrent(path, trackers, infos, source, chunk_size, seed=true, private_tracker=false) {}
+function limit_download(v) {}
+function limit_upload(v) {}
+function retracker(t, trackers) {}
+
+function update_system_infos(data) {
+    hdd=document.getElementById('hdd');
+    cpu=document.getElementById('cpu');
+    ram=document.getElementById('ram');
+    //TODO: read data, change title attribute & update UI with blue-green-red gradient
+}
+
+function notify(){}
+
+function update_log(data) {}
--- app/static/js/ui.js
+++ app/static/js/ui.js
@@ -1,15 +1,58 @@
-function add_torrent_dialog() {
-
+/* Dialogs */
+function add_torrent_dialog(e) {
+    alert("Add a torrent");
 }
-
 function manage_feed_dialog() {
 
 }
-
 function manage_premium_account_dialog() {
     
 }
-
 function edit_torrent() {
+    
+}
+function shortcut_dialog() {
+    document.getElementById('keyboard_shortcuts').style.display='block';
+}
+function settings_dialog() {
+
+}
+function show_explorer() {
+    
+}
+function show_connections() {
+    
+}
+function show_history() {
+    
+}
+function create_torrent_dialog() {
+    
+}
+/* Right-click menu */
+function set_global_download_speed() {
+
+}
+function set_global_upload_speed() {
+
+}
+
+/* Filter functions */
+function clear_filters() {
+    document.getElementById('search').value='';
+}
+function filter_status(s) {
+
+}
+function filter_label(l) {
+
+}
+function filter_tracker(s) {
+
+}
+function filter_user(u) {
+
+}
+function search(txt) {
     
 }
