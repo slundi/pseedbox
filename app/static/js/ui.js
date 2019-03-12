db92171162d8209b1c4dddae7e3af23c21022b51
slundi <slundi@gmail.com>
2019-03-12 22:51:50 +0100

    Starting implementing UI events

--- app/static/js/ui.js
+++ app/static/js/ui.js
@@ -19,8 +19,11 @@ function set_global_upload_speed() {
 }
 
 /* Filter functions */
-function clear_filters() {
-    document.getElementById('search').value='';
+function clear_filters(searchbox=true) {
+    console.log('Clear filters')
+    if(searchbox) document.getElementById('search').value='';
+    var filters = document.getElementsByClassName("filter");
+    Array.prototype.forEach.call(filters, function(e){e.setAttribute('class', 'filter');});
 }
 function filter_status(s) {
 
@@ -37,3 +40,18 @@ function filter_user(u) {
 function search(txt) {
     
 }
+/* Events */
+document.addEventListener('click', function(event) {
+    //close opened modal
+    var modals = document.getElementsByClassName("modal");
+    Array.prototype.forEach.call(modals, function(e){if(e.style.display!='none' && !e.firstChild.contains(event.target)) e.style.display='none';});
+    //filters
+    var filters = document.getElementsByClassName("filter");
+    Array.prototype.forEach.call(filters, function(e){
+        if(e.contains(event.target)) {
+            console.log('It\'s me... a filter')
+            clear_filters();
+            e.setAttribute('class', 'filter active');
+        }
+    });
+});
